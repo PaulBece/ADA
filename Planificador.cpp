0f425122d480abfb596d773bdc8430d951ab1049
Paul Becerra <paulbecerracs@gmail.com>
2024-05-29 23:13:22 -0500

    add Planificador

--- Planificador.cpp
+++ Planificador.cpp
@@ -17,40 +17,33 @@ float planificador(std::vector<float> &b, std::vector<int> &d, std::vector<int> 
 		bp[i].a = b[i];
 		bp[i].i = i;
 	}
-	/*for (auto& it : bp) {
-		std::cout << it.a << " ";
-	}*/
 	std::sort(bp.begin(), bp.end(),menor);
 	
-	int c = 0;
-	for (int i = 0; i < S.size()+c; i++) {
-		int aux = 0;
-		while (1) {
-			if (&(S[bp[i].i - aux]) < &S[0]) {
+	for (int i = 0, c = 0; c < S.size() && i<bp.size() ; i++) {
+		int pos = d[bp[i].i]-1;
+		while (pos >= 0) {
+			if ((S[pos])) {
+				pos--;
+			}
+			else {
+				S[pos] = bp[i].i + 1;
+				bTotal += b[bp[i].i];
 				c++;
 				break;
 			}
-			if ((S[bp[i].i - aux])) {
-				aux++;
-			}
-			else {
-				S[bp[i].i - aux] = bp[i].i + 1;
-				bTotal += b[bp[i].i];
-			}
 		}
 	}
-	
 	return bTotal;
 }
 
 int  main() {
 	int T = 3;
+	std::vector<int> S(T);
 	std::vector<float> b = { 20, 15, 10, 7, 5, 3 };
 	std::vector<int> d = { 3, 1, 1, 3, 1, 3 };
-	std::vector<int> S(T);
 	float bTotal = planificador(b, d, S);
 	std::cout << "Beneficio Total = " << bTotal << "\n";
-	std::cout << "Proporcion de elementos escogidos: ";
+	std::cout << "Orden de tareas escogidas: ";
 	for (auto& it : S) {
 		std::cout << it << " ";
 	}
